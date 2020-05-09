d808d40d27b599b15e47b1a4ad829da95ce431fc
Mahadevan P G <Mahadevan.PG@macys.com>
2020-05-09 13:10:43 +0530

    Import for Cross Origin

--- src/main/java/com/macys/survey/controller/SurveyResponseController.java
+++ src/main/java/com/macys/survey/controller/SurveyResponseController.java
@@ -10,6 +10,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 import java.util.HashMap;
 import java.util.List;
